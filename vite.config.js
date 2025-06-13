6590d5928372948c619abaaf4b21a626c9214b5a
Mike <myklee@gmail.com>
2025-06-14 03:45:07 +0800

    github pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/rcbuildlog/' : '/',
   server: {
     port: 3333,  // Replace with the port you want to use
   },
